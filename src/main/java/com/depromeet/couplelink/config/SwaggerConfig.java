37c94efe07264438d19b00454f51dd102daf1cf2
전해성 <seong0428@gmail.com>
2019-05-31 23:25:48 +0900

    resolve #3 - swagger 의존성, 설정을 추가합니다

---
+++ src/main/java/com/depromeet/couplelink/config/SwaggerConfig.java
@@ -0,0 +1,9 @@
+package com.depromeet.couplelink.config;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+}
