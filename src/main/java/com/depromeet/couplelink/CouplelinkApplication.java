d88231f3d9e3d6915dd17f2a7624bc8f8fd8661d
전해성 <seong0428@gmail.com>
2019-05-31 22:24:32 +0900

    Initial commit

---
+++ src/main/java/com/depromeet/couplelink/CouplelinkApplication.java
@@ -0,0 +1,13 @@
+package com.depromeet.couplelink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouplelinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouplelinkApplication.class, args);
+	}
+
+}
