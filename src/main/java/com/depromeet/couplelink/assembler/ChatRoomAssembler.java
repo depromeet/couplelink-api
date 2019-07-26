77ffdeecbac0b20997a5b1aa7607626760f3fdeb
Haeseong Jeon <seong0428@gmail.com>
2019-07-27 00:54:18 +0900

    fix - 채팅방 null 일 때 500 에러 발생하는 현상 수정

--- src/main/java/com/depromeet/couplelink/assembler/ChatRoomAssembler.java
+++ src/main/java/com/depromeet/couplelink/assembler/ChatRoomAssembler.java
@@ -3,17 +3,16 @@ package com.depromeet.couplelink.assembler;
 import com.depromeet.couplelink.dto.ChatRoomResponse;
 import com.depromeet.couplelink.entity.ChatRoom;
 import org.springframework.stereotype.Component;
-import org.springframework.util.Assert;
 
 @Component
 public class ChatRoomAssembler {
     public ChatRoomResponse assembleChatRoomResponse(ChatRoom chatRoom) {
-        Assert.notNull(chatRoom, "'chatRoom' must not be null");
-
+        if (chatRoom == null) {
+            return null;
+        }
         final ChatRoomResponse chatRoomResponse = new ChatRoomResponse();
         chatRoomResponse.setId(chatRoom.getId());
         chatRoomResponse.setName(chatRoom.getName());
-
         return chatRoomResponse;
     }
 }
